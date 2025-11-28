a333bdcae57d3544aa64f70972a119822fa8cae4
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-11-28 17:14:45 +0300

    refactor(logging): replace staff_logs with action_logs and improve action_type

--- database/migrations/2025_11_28_135329_create_action_logs_table.php
+++ database/migrations/2025_11_28_135329_create_action_logs_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\ActionType;
 
 return new class extends Migration
 {
@@ -11,22 +12,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('staff_logs', function (Blueprint $table) {
+        Schema::create('action_logs', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->enum('action_type', [
-                'order_created',
-                'order_status_changed',
-                'order_updated',
-                'order_item_created',
-                'order_item_status_changed',
-                'order_item_updated',
-                'product_created',
-                'product_updated',
-                'product_status_changed',
-                'user_created',
-                'user_updated'
-            ]);
+            $table->enum('action_type', array_column(ActionType::cases(), 'value'));
             $table->string('entity_type'); // 'App\\Models\\Order', 'App\\Models\\Product'
             $table->unsignedBigInteger('entity_id');
             $table->json('old_values')->nullable(); // данные до изменения
@@ -48,6 +37,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('staff_logs');
+        Schema::dropIfExists('action_logs');
     }
 };

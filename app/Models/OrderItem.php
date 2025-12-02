38737397d16a36cc5ec8371707a5c85b76cd1971
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-12-03 00:13:43 +0300

    feat(models): add product relationship to OrderItem

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -25,4 +25,9 @@ class OrderItem extends Model
     {
         return $this->belongsTo(Order::class);
     }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }

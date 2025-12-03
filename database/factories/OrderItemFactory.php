d122a60a6446f264668bf249f5b32ff7ea32bfc4
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-12-04 01:21:23 +0300

    refactor(factory): update OrderItemFactory with enum and conditional logic

--- database/factories/OrderItemFactory.php
+++ database/factories/OrderItemFactory.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User;
 use App\Models\Role;
 use App\Models\Product;
+use App\Enums\OrderItemStatus;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderItem>
@@ -19,25 +20,32 @@ class OrderItemFactory extends Factory
      */
     public function definition(): array
     {
+        $status = $this->faker->randomElement(
+                array_column(OrderItemStatus::cases(), 'value')
+        );
+
+        $is_pending = $status === OrderItemStatus::PENDING->value;
+        $is_in_delivery = $status === OrderItemStatus::IN_DELIVERY->value;
+        $is_served = $status === OrderItemStatus::SERVED->value;
+        $is_cancelled = $status === OrderItemStatus::CANCELLED->value;
+
         return [
             'product_id' => Product::inRandomOrder()->first()->id,
             'unit_price' => $this->faker->randomFloat(2, 5, 500),
-            'status' => $this->faker->randomElement([
-                'pending',
-                'preparing',
-                'ready',
-                'served',
-                'cancelled'
-            ]),
-            'cook_id' => User::where('role', Role::class::where('name', 'cook')->first()->id)
-                ->inRandomOrder()
-                ->first()
-                ->id,
-            'served_by' => User::where('role', Role::class::where('name', 'waiter')->first()->id)
-                ->inRandomOrder()
-                ->first()
-                ->id,
-            'served_at' => $this->faker->optional()->dateTimeBetween('-2 days', 'now'),
+            'status' => $status,
+            'cook_id' => !($is_pending || $is_cancelled)
+                ? User::where('role_id', Role::class::where('name', 'cook')->first()->id)
+                    ->inRandomOrder()
+                    ->first()
+                    ->id
+                : null,
+            'served_by' => ($is_in_delivery || $is_served)
+                ? User::where('role_id', Role::class::where('name', 'waiter')->first()->id)
+                    ->inRandomOrder()
+                    ->first()
+                    ->id
+                : null,
+            'served_at' => $is_served ? $this->faker->dateTimeBetween('-2 days', 'now') : null,
             'notes' => $this->faker->optional()->sentence(),
         ];
     }

7c34c9b30aa448bf419b118c26ce74bd4d9ddf6e
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-12-04 01:35:35 +0300

    refactor(seeder): update OrderSeeder with withoutEvents

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Order;
+
 
 class OrderSeeder extends Seeder
 {
@@ -12,9 +14,11 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Order::factory()
-            ->count(10)
-            ->hasOrderItems(3)
-            ->create();
+        Order::withoutEvents(function(){
+            Order::factory()
+                ->count(10)
+                ->hasItems(3)
+                ->create();
+        });
     }
 }

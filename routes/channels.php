92a51c51b3fe13becf69c3fb0d5bf2493884fe15
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-12-25 15:38:32 +0300

    feat(broadcasting): define channels for cooks

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use App\Models\User;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('cooks.pending-items', function (User $user) {
+    return $user->hasRole('cook');
+});
+
+Broadcast::channel('user.{id}.preparing-items', function (User $user, $id) {
+    return (int) $user->id === (int) $id && $user->hasRole('cook');
 });

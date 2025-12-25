c2d14ffe62414e377d20574ff71ab800296bd79b
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-12-25 15:40:03 +0300

    feat(broadcasting): define channels for waiters

--- routes/channels.php
+++ routes/channels.php
@@ -10,3 +10,11 @@ Broadcast::channel('cooks.pending-items', function (User $user) {
 Broadcast::channel('user.{id}.preparing-items', function (User $user, $id) {
     return (int) $user->id === (int) $id && $user->hasRole('cook');
 });
+
+Broadcast::channel('waiters.ready-items', function (User $user) {
+    return $user->hasRole('waiter');
+});
+
+Broadcast::channel('user.{id}.delivery-items', function (User $user, $id) {
+    return (int) $user->id === (int) $id && $user->hasRole('waiter');
+});

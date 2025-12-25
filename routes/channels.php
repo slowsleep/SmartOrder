83be441a5faae5a1299f0f10509a445c469d16e4
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-12-25 15:41:06 +0300

    feat(broadcasting): define channels for clients

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Broadcast;
 use App\Models\User;
+use App\Models\Order;
 
 Broadcast::channel('cooks.pending-items', function (User $user) {
     return $user->hasRole('cook');
@@ -17,4 +18,19 @@ Broadcast::channel('waiters.ready-items', function (User $user) {
 
 Broadcast::channel('user.{id}.delivery-items', function (User $user, $id) {
     return (int) $user->id === (int) $id && $user->hasRole('waiter');
+});
+
+Broadcast::channel('order.{orderId}', function (User $user, $orderId) {
+    if ($user) {
+        return $user->hasRole('admin') || $user->hasRole('cook') || $user->hasRole('waiter');
+    }
+
+    $sessionGuestToken = session()->get("order_{$orderId}_token");
+    $order = Order::where('id', $orderId)->where('guest_token', $sessionGuestToken)->first();
+
+    if (!$order) {
+        return false;
+    }
+
+    return true;
 });

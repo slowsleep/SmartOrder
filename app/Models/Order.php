39cb49623233ee619513d990561d08eba68ae294
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-12-25 15:08:15 +0300

    feat(models): add table relationship to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -28,4 +28,9 @@ class Order extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+
+    public function table()
+    {
+        return $this->belongsTo(Table::class);
+    }
 }
